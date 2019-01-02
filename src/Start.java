058a699a8c89f8c639ac057e9b29cbb8793dce9d
damian <test@test.com>
2019-01-02 12:09:51 +0100

    dodanie nowego produktu

--- src/Start.java
+++ src/Start.java
@@ -5,9 +5,12 @@ public class Start {
 
         Produkt poszewka = new Produkt("poszewka", "materiał", 20L);
         Produkt kokarda = new Produkt ("kokarda", "materiał", 10L);
+        Produkt zamek = new Produkt("zamek", "zamek błyskawiczny", 10L);
         Koszyk koszyk = new Koszyk ();
         koszyk.dodajProdukt(poszewka);
         koszyk.dodajProdukt(kokarda);
+        koszyk.dodajProdukt(zamek);
+
 
         System.out.println("Wartość koszyka: "+koszyk.pokazCena());
     }
